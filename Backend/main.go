c12bc8ed7e737a19dac1f86bf662bd6126a4b310
Anshydv18 <anshgithub@gmail.com>
2024-11-21 23:43:32 +0530

    basic setup

--- Backend/main.go
+++ Backend/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/get", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Message": "started",
+		})
+	})
 }
