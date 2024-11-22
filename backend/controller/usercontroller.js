6a29780e15cd3e66fb4260849398c71596c71f99
Anshydv18 <anshgithub@gmail.com>
2024-11-22 22:07:04 +0530

    sign up controller

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -1,7 +1,8 @@
-import User from "."
+import User from "../models/userModel.js"
+import bcrypt from "bcryptjs"
 export const Signup = async(req,res)=>{
     try {
-        const {name,email,age,gender,password,confirmpassword}=req.body;
+        const {name,username,email,age,gender,password,confirmpassword}=req.body;
 
         if(password!=confirmpassword){
             return res.Status(400).json({
@@ -9,9 +10,38 @@ export const Signup = async(req,res)=>{
             })
         }
 
-        const user = await 
-        
+        const user = await User.findOne({email})
+        if(user){
+            res.Status(400).json({
+                error:"email already in use"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            name,
+            username,
+            email,
+            age,
+            gender,
+            password:hashedPassword,
+            password
+        })
+
+        if(!newUser){
+            res.Status(400).json({
+                error:"Not able to create , please retry"
+        })}
+
+        await newUser.save()
+        return res.Status(200).json({
+            message : "user created sucessfully 😍"
+        })
     } catch (error) {
-        
+        return res.Status(500).json({
+            error:"Server is not responding"
+        })
     }
 }
